0ee6912d7a9017cb579ac70c68570e779bbe303c
SimaoBranda <sibrandao2008@gmail.com>
2025-12-26 21:37:07 +0100

    first commit

---
+++ Bi4AppCSharpDesktop/Entidades.cs
@@ -0,0 +1,22 @@
+namespace Bi4AppCSharpDesktop
+{
+    public class Entidades
+    {
+        public string NomeCompleto { get; set; }
+        public string NumeroBilhete { get; set; }
+        public string Provincia { get; set; }
+        public string DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public string EstadoCivil { get; set; }
+        public string DataEmissao { get; set; }
+        public string DataValidade { get; set; }
+        public string ProvinciaEmissora { get; set; }
+        public string Versao { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+        public int Idade { get; set; }
+        public string EstadoValidade { get; set; }
+        public string FaixaEtaria { get; set; }
+        public string DiasAteExpiracao { get; set; }
+    }
+}
